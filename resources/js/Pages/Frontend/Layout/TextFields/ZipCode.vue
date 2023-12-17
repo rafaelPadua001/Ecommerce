a42ebe29da4b12c8162ced0145c4ed3d369894fb
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2023-12-17 13:07:03 -0300

    Fix:
    Tabs on productdialog

--- resources/js/Pages/Frontend/Layout/TextFields/ZipCode.vue
+++ resources/js/Pages/Frontend/Layout/TextFields/ZipCode.vue
@@ -1,121 +1,113 @@
 <template>
-   
     <v-row>
-        <v-col class="d-flex justify-center mb-2 flex-column" cols="12" md="8" sm="4">
+        <v-col class="d-flex justify-center mb-2 flex-column" cols="12" sm="8" md="8">
             <v-card class="mx-auto" width="350px">
                 <v-card-title>Deliveries</v-card-title>
                 <v-toolbar class="px-0" color="transparent">
                     <template v-slot:extension>
-                        <v-tabs
-                            v-model="tabs"
-                            v-for="(delivery, index) in deliveries"
-                            :key="delivery.id"
-                            :value="index"
+                        <v-tabs 
+                            v-model="tabs" 
                             color="primary"
-                            grow
-                            
+                            grow v-for="(delivery, index) in deliveries" 
+                            :key="index"
+                            :value="index"
                         >
-                            <v-tab
-                                :value="index"  
-                                v-if="delivery.activated == 1"
-                            >
+                            <v-tab v-if="delivery.activated == 1 && index >= 0">
                                 <v-img
+                                    justify="left" 
                                     :src="`./storage/delivery/${delivery.thumbnail}`"
-                                    :lazy-src="`./storage/delivery/${delivery.thumbnail}`"
+                                    :lazy-src="`./storage/delivery/${delivery.thumbnail}`" 
                                     cover
                                     :aspect-ratio="1/1"
-                                    :width="25"
-                                    :max-height="50"
+                                    :width="40" :max-height="40" 
                                 >
-
-                                </v-img>
-                                {{ delivery.name }}
-                                
-                            </v-tab>
-                        </v-tabs>
-                    </template>  
-                </v-toolbar>
-                
-                <v-card-text>
-                    
-                    <v-window v-model="tabs" >   
-                        <v-window-item
-                            v-for="(delivery, index) in deliveries"
-                            :key="delivery.id"
-                            :value="index"
-                           
-                        >
-                        <v-row v-model="delivery.id"  v-if="delivery.activated == 1">
-                        <v-col class="d-flex child-flex" cols="12" md="8" sm="3">
-                            <v-text-field v-model="zip_code" v-maska:[options] label="zip code" aria-required>
-
-                            </v-text-field>
-                        </v-col>
-                        <v-col cols="2">
-
-                            <v-btn variant="text" size="x-small" @click="calculateDelivery(delivery)">Calcular frete</v-btn>
-
-                        </v-col>
-                    </v-row>
-                        <v-list>
-                <v-list-item v-for="shipping in shipping_companys" :key="shipping.id">
-                    <div v-if="!shipping.error" v-for="delivery in shipping" :key="delivery.id">
-                        <div v-if="!delivery.error">
-                            <v-row no-gutters>
-                                <v-col cols="auto">
                                     
-                                    <v-radio-group v-model="selectedShippment">
-                                        <v-radio :value="delivery" @change="selectShippment">
-                                                        
-                                    </v-radio>
-                                </v-radio-group>
-                          
+                                </v-img>
+
+                               
+                           </v-tab>
+                        </v-tabs>
+                    </template>
+                </v-toolbar>
+
+                <v-card-text>
+
+                    <v-window v-model="tabs">
+                        <v-window-item v-for="(delivery, index) in deliveries" :key="index" :value="index">
+                            <v-row v-model="delivery.id" v-if="delivery.activated == 1">
+                                <v-col class="d-flex child-flex" cols="12" md="8" sm="3">
+                                    <v-text-field v-model="zip_code" v-maska:[options] label="zip code" aria-required>
+
+                                    </v-text-field>
                                 </v-col>
-                                <v-col>
-                                    <v-list-item-avatar>
-                                            <v-img :max-width="200" :max-height="25" :src="delivery.company.picture" :lazy-src="delivery.company.picture"
-                                                :placeholder="delivery.company.name">
-                                            </v-img>
-                                    </v-list-item-avatar>
+                                <v-col cols="2">
+
+                                    <v-btn variant="text" size="x-small" @click="calculateDelivery(delivery)">Calcular
+                                        frete</v-btn>
+
                                 </v-col>
                             </v-row>
-                          
-                               
-                           
-                            <p class="text-body">
-                                {{ delivery.name }}
-                                <span>
-                                    Price: {{ delivery.currency }}: {{ delivery.price }}
-                                </span>
-                            </p>
+                            <v-list>
+                                <v-list-item v-for="shipping in shipping_companys" :key="shipping.id">
+                                    <div v-if="!shipping.error" v-for="delivery in shipping" :key="delivery.id">
+                                        <div v-if="!delivery.error">
+                                            <v-row no-gutters>
+                                                <v-col cols="auto">
 
-                            <p class="text-body">
-                                <span>
-                                    Prazo de Entrega:
-                                    até {{ delivery.delivery_time }} dias uteis
-                                </span>
-                            </p>
-                        </div>
-                    </div>
-                    <div v-else>
-                        Error
+                                                    <v-radio-group v-model="selectedShippment">
+                                                        <v-radio :value="delivery" @change="selectShippment">
 
-                    </div>
+                                                        </v-radio>
+                                                    </v-radio-group>
 
-                    <template v-slot:prepend>
+                                                </v-col>
+                                                <v-col>
+                                                    <v-list-item-avatar>
+                                                        <v-img :max-width="200" :max-height="25"
+                                                            :src="delivery.company.picture"
+                                                            :lazy-src="delivery.company.picture"
+                                                            :placeholder="delivery.company.name">
+                                                        </v-img>
+                                                    </v-list-item-avatar>
+                                                </v-col>
+                                            </v-row>
 
-                    </template>
-                   
 
-                </v-list-item>
-                
-            </v-list>
+
+                                            <p class="text-body">
+                                                {{ delivery.name }}
+                                                <span>
+                                                    Price: {{ delivery.currency }}: {{ delivery.price }}
+                                                </span>
+                                            </p>
+
+                                            <p class="text-body">
+                                                <span>
+                                                    Prazo de Entrega:
+                                                    até {{ delivery.delivery_time }} dias uteis
+                                                </span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div v-else>
+                                        Error
+
+                                    </div>
+
+                                    <template v-slot:prepend>
+
+                                    </template>
+
+
+                                </v-list-item>
+
+                            </v-list>
                         </v-window-item>
                     </v-window>
-                   
+
                 </v-card-text>
             </v-card>
-            
+
         </v-col>
     </v-row>
 </template>
@@ -134,8 +126,7 @@ const myValue = ref('');
 export default {
     props: ['selectProduct', 'quantity'],
     data: () => ({
-        tabs: true,
-        tabs2: true,
+        tabs: null,
         deliveries: [],
         zip_code: '',
         shippment: false,
@@ -144,19 +135,19 @@ export default {
         delivery_name: false,
     }),
     methods: {
-        getDeliveries(){
+        getDeliveries() {
             axios.get('/api/delivery')
-            .then((response) => {
-                return this.deliveries = response.data;
-            })
-            .catch((response) => {
-                return alert('Error :' + response);
-            });
+                .then((response) => {
+                    return this.deliveries = response.data;
+                })
+                .catch((response) => {
+                    return alert('Error :' + response);
+                });
         },
         calculateDelivery(delivery) {
             const deliveryId = delivery.id;
             this.delivery_name = delivery.name;
-           
+
             const data = {
                 postal_code: this.zip_code,
                 height: this.selectProduct.height,
@@ -175,7 +166,7 @@ export default {
                     return alert('Error: ', response.error);
                 })
         },
-        selectShippment(){
+        selectShippment() {
             console.log('Dados a serem enviados para o pai:', this.selectedShippment);
             return this.$emit('updateShippment', this.selectedShippment, this.zip_code, this.delivery_name);
         }
@@ -183,6 +174,6 @@ export default {
     created() {
         this.getDeliveries();
     }
-    
+
 }
 </script>
