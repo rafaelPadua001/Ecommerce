52f0dd64b8c8f313a400c0f90cf583581761dc00
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-01-03 14:13:41 -0300

    Feat:
    Create datable items, and remove coupon button, dialog to remove.

---
+++ resources/js/Pages/Frontend/Coupons/partials/Remove.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-dialog>
+        <v-card>
+            <v-card-title>
+                <v-toolbar class="bg-transparent">
+                    <v-toolbar-title>
+                        Remove Coupon
+                    </v-toolbar-title>
+                    <template v-slot:append>
+                        <v-btn icon variant="plain">
+                            <v-icon icon="fas fa-close"></v-icon>
+                        </v-btn>
+                        
+                    </template>
+                </v-toolbar>
+               
+            </v-card-title>
+            <v-card-text>
+                Aqui será o dialog de remoção de cupon
+            </v-card-text>
+
+            <v-card-actions>
+                <v-btn-group>
+                    <v-btn class="mr-2" variant="text">
+                        cancel
+                    </v-btn>
+                    <v-btn class="mr-2" color="error" variant="text">
+                        Remove
+                    </v-btn>
+                    
+                </v-btn-group>
+            </v-card-actions>
+        </v-card>
+      
+    </v-dialog>
+   
+</template>
