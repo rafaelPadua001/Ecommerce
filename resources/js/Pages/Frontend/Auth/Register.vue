d55413fbd2644457a7ca00abd1fdb5afe832de56
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-01-04 13:15:47 -0300

    Fix:
    Layout to register and login

--- resources/js/Pages/Frontend/Auth/Register.vue
+++ resources/js/Pages/Frontend/Auth/Register.vue
@@ -1,133 +1,139 @@
 <template>
     <div>
-        <v-row no-gutters>
-            <v-col class="d-flex justify-center mb-6 flex-column" cols="12" md="12" sm="4">
-                <v-responsive>
-                    <v-card class="bg-transparent-accent-4 mx-auto" width="800" elevation="0">
+        <v-container>
+            <v-row no-gutters>
+            <v-col class="d-flex justify-center mb-6 flex-column" cols="auto" md="12" sm="6">
+                <v-sheet>
+                    <v-card class="bg-transparent-accent-4 mx-auto" :width="800" elevation="0">
                         <v-card-text>
                             <v-row>
-                                <v-col cols="6">
-                                    <v-card class="mx-auto" elevation="0">
-                                        <v-img :width="400" height="500" src="https://picsum.photos/1920/1080?random"
-                                            aspect-ratio="16/9" cover class="text-white">
+                                <v-col cols="6" md="6">
+                                    
+                                        <v-img width="410" height="500" aspect-ratio="4/3" cover src="https://picsum.photos/1920/1080?random"
+                                            class="text-white">
                                         </v-img>
-                                    </v-card>
+                                   
                                 </v-col>
 
-                                <v-col cols="6">
-                                    <v-card class="mx-auto" elevation="0">
-                                        <v-card-text>
+                                <v-col cols="auto" md="5">
+                                    <v-row>
+                                        <v-col>
+                                            <v-img 
+                                                class="mx-auto my-2"
+                                                max-width="150"
+                                                src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg">
+                                            </v-img>
+                                        </v-col>
+                                    </v-row>
+                                    <v-row>
+                                        <v-col>
                                             <v-form>
-                                                <v-img class="mx-auto my-2" max-width="228"
-                                                    src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg">
-                                                </v-img>
+                                               
 
-                                                <v-divider></v-divider>
-                                                <v-card-text>
-                                                    <v-row no-gutters>
-                                                        <v-col class="d-flex justify-center flex-column" cols="auto">
-                                                            <div v-if="!error">
-                                                                <v-alert title="success !" :text="message" v-model="alert"
-                                                                    type="success">
-                                                                </v-alert>
-                                                            </div>
+                                               <v-divider></v-divider>
+                                               <v-card-text>
+                                                   <v-row no-gutters>
+                                                       <v-col class="d-flex justify-center flex-column" cols="auto">
+                                                           <div v-if="!error">
+                                                               <v-alert title="success !" :text="message" v-model="alert"
+                                                                   type="success">
+                                                               </v-alert>
+                                                           </div>
 
-                                                            <div v-else>
-                                                                <v-alert type="error" :text="error" v-model="alert">
-                                                                </v-alert>
-                                                            </div>
-                                                        </v-col>
-                                                    </v-row>
-                                                    <v-row>
-                                                        <v-col class="d-flex justify-center flex-column" cols="12" md="12"
-                                                            sm="4">
+                                                           <div v-else>
+                                                               <v-alert type="error" :text="error" v-model="alert">
+                                                               </v-alert>
+                                                           </div>
+                                                       </v-col>
+                                                   </v-row>
+                                                   <v-row>
+                                                       <v-col class="d-flex justify-center flex-column" cols="12" md="12"
+                                                           sm="4">
 
-                                                            <v-text-field ref="first_name" v-model="first_name"
-                                                                :rules="[() => !!first_name || 'This field is required']"
-                                                                :error-messages="errorMessages" label="First Name"
-                                                                placeholder="John" required></v-text-field>
+                                                           <v-text-field ref="first_name" v-model="first_name"
+                                                               :rules="[() => !!first_name || 'This field is required']"
+                                                               :error-messages="errorMessages" label="First Name"
+                                                               placeholder="John" required></v-text-field>
 
 
-                                                        </v-col>
-                                                    </v-row>
-                                                    <v-row>
-                                                        <v-col class="d-flex justify-center flex-column">
+                                                       </v-col>
+                                                   </v-row>
+                                                   <v-row>
+                                                       <v-col class="d-flex justify-center flex-column">
 
-                                                            <v-text-field ref="last_name" v-model="last_name"
-                                                                :rules="[() => !!last_name || 'This field is required']"
-                                                                :error-messages="errorMessages" label="Last Name"
-                                                                placeholder="Doe" required></v-text-field>
+                                                           <v-text-field ref="last_name" v-model="last_name"
+                                                               :rules="[() => !!last_name || 'This field is required']"
+                                                               :error-messages="errorMessages" label="Last Name"
+                                                               placeholder="Doe" required></v-text-field>
 
-                                                        </v-col>
-                                                    </v-row>
+                                                       </v-col>
+                                                   </v-row>
 
-                                                    <v-row>
-                                                        <v-col class="d-flex justify-center flex-column">
-
-
-                                                            <v-text-field v-model="email" name="email" density="compact"
-                                                                prepend-inner-icon="mdi-email-outline" :counter="15"
-                                                                label="Email:" placeholder="johndoe@email.com" type="email"
-                                                                required hide-details="auto">
-
-                                                            </v-text-field>
-
-                                                        </v-col>
-
-                                                    </v-row>
-                                                    <v-row>
-                                                        <v-col class="d-flex justify-center flex-column">
-                                                            <v-text-field v-model="password"
-                                                                append-icon="show1 ? 'fas fa-eye' : 'fas fa-eye-off'"
-                                                                name="password" density="compact"
-                                                                placeholder="Enter your password"
-                                                                :rules="[rules.required, rules.min]"
-                                                                :type="show1 ? 'text' : 'password'" :counter="8"
-                                                                label="Password" hint="at last 8 characters" counter
-                                                                @click:prepend="show1 = !show1">
-
-                                                            </v-text-field>
-                                                        </v-col>
-                                                    </v-row>
+                                                   <v-row>
+                                                       <v-col class="d-flex justify-center flex-column">
 
 
-                                                </v-card-text>
+                                                           <v-text-field v-model="email" name="email" density="compact"
+                                                               prepend-inner-icon="mdi-email-outline" :counter="15"
+                                                               label="Email:" placeholder="johndoe@email.com" type="email"
+                                                               required hide-details="auto">
 
-                                                <v-divider></v-divider>
-                                                <v-card-actions>
-                                                    <v-btn variant="text" href="/">
-                                                        Cancel
-                                                    </v-btn>
-                                                    <v-spacer></v-spacer>
-                                                    <v-slide-x-reverse-transition>
-                                                        <v-tooltip v-if="formHasErrors" location="left">
-                                                            <template v-slot:activator="{ on, attrs }">
-                                                                <v-btn icon class="my-0" v-bind="attrs" @click="resetForm"
-                                                                    v-on="on">
-                                                                    <v-icon>mdi-refresh</v-icon>
-                                                                </v-btn>
-                                                            </template>
-                                                            <span>Refresh form</span>
-                                                        </v-tooltip>
-                                                    </v-slide-x-reverse-transition>
-                                                    <v-btn color="primary" variant="text" @click="submit">
-                                                        Submit
-                                                    </v-btn>
-                                                </v-card-actions>
-                                            </v-form>
-                                        </v-card-text>
-                                    </v-card>
+                                                           </v-text-field>
 
+                                                       </v-col>
+
+                                                   </v-row>
+                                                   <v-row>
+                                                       <v-col class="d-flex justify-center flex-column">
+                                                           <v-text-field v-model="password"
+                                                               append-icon="show1 ? 'fas fa-eye' : 'fas fa-eye-off'"
+                                                               name="password" density="compact"
+                                                               placeholder="Enter your password"
+                                                               :rules="[rules.required, rules.min]"
+                                                               :type="show1 ? 'text' : 'password'" :counter="8"
+                                                               label="Password" hint="at last 8 characters" counter
+                                                               @click:prepend="show1 = !show1">
+
+                                                           </v-text-field>
+                                                       </v-col>
+                                                   </v-row>
+
+
+                                               </v-card-text>
+
+                                               <v-divider></v-divider>
+                                               <v-card-actions>
+                                                   <v-btn variant="text" href="/">
+                                                       Cancel
+                                                   </v-btn>
+                                                   <v-spacer></v-spacer>
+                                                   <v-slide-x-reverse-transition>
+                                                       <v-tooltip v-if="formHasErrors" location="left">
+                                                           <template v-slot:activator="{ on, attrs }">
+                                                               <v-btn icon class="my-0" v-bind="attrs" @click="resetForm"
+                                                                   v-on="on">
+                                                                   <v-icon>mdi-refresh</v-icon>
+                                                               </v-btn>
+                                                           </template>
+                                                           <span>Refresh form</span>
+                                                       </v-tooltip>
+                                                   </v-slide-x-reverse-transition>
+                                                   <v-btn color="primary" variant="text" @click="submit">
+                                                       Submit
+                                                   </v-btn>
+                                               </v-card-actions>
+                                           </v-form>
+                                        </v-col>
+                                    </v-row>
                                 </v-col>
                             </v-row>
                         </v-card-text>
-
                     </v-card>
-                </v-responsive>
+                </v-sheet>
             </v-col>
         </v-row>
-
-    </div>
+    </v-container>
+</div>
 </template>
 
 <script>
