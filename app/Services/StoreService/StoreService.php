e0fb8d8da8eca44301f23fba3bc9f03ee5370f91
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-01-23 22:30:09 -0300

    Feat
    Create Style store function, to recorder attributes to style main page.

--- app/Services/StoreService/StoreService.php
+++ app/Services/StoreService/StoreService.php
@@ -8,23 +8,27 @@ use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
-class StoreService {
+class StoreService
+{
     protected $store;
 
-    public function __construct(Store $store){
+    public function __construct(Store $store)
+    {
         $this->store = $store;
     }
-    public function getStore(){
+    public function getStore()
+    {
         $store = $this->store->first();
         return $store;
     }
-    public function store(Request $request){
-        try{
-            
+    public function store(Request $request)
+    {
+        try {
+
             $file = $request->file('app_logo');
             $upload_image = $this->uploadImg($file);
             $user = Auth::user();
-           
+
             $store = $this->store->create([
                 'app_name' => $request->app_name,
                 'app_mail' => $request->app_mail,
@@ -33,41 +37,82 @@ class StoreService {
                 'app_address' => $request->app_address,
                 'user_id' => $user->id
             ]);
-          
+
             return $store;
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             return $e;
         }
-        
     }
-    public function uploadImg($file){
-      
-        foreach($file as $img){
-            $fileName = 'Logos/app_icon.'.$img->Extension();
-            
+    public function uploadImg($file)
+    {
+
+        foreach ($file as $img) {
+            $fileName = 'Logos/app_icon.' . $img->Extension();
+
             $path = Storage::putFileAs('/public/app_icon/', $img, $fileName);
 
-           return $fileName;
+            return $fileName;
         }
     }
-    public function updateStore($id, $request){
-        try{
-           
+    public function updateStore($id, $request)
+    {
+        try {
+
             $updateStore = $this->store->findOrFail($id)->update($request->all());
             //dd($request->all());
             return $request->all();
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             return $e;
         }
     }
-    public function destroy($id){
-        try{
+    public function styleStore($request)
+    {
+
+        try {
+            $banner1 = $request->banner1[1];
+
+            $uploadBannerImage = $this->uploadImgBanner($banner1);
+
+            $carrousel = [
+                'banner2' => $request->banner2[1],
+                'banner3' => $request->banner3[1],
+                'banner4' => $request->banner4[1],
+                'banner5' => $request->banner5[1]
+            ];
+            $upload_carrousel = $this->uploadImgCarrousel($carrousel);
+            dd($upload_carrousel);
+            return response()->json($upload_carrousel);
+        } catch (Exception $e) {
+            return $e;
+        }
+    }
+    public function uploadImgBanner($file)
+    {
+        foreach ($file as $img) {
+
+            $fileName  = 'Banner1.webp';
+            $path = Storage::putFileAs('/public/Banners/', $img, $fileName);
+            return $fileName;
+        }
+    }
+    public function uploadImgCarrousel($carrousel)
+    {
+
+        foreach ($carrousel as $img) {
+
+            $fileName  = $img['name'];
+
+            $path = Storage::putFileAs('/public/Carrousel/', $img['src'], $fileName);
+        }
+
+        return $fileName;
+    }
+    public function destroy($id)
+    {
+        try {
             $destroy = $this->store->findOrFail($id)->delete();
             return $destroy;
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             return response()->json($e);
         }
     }
