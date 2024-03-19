f7d6c10bd5c3ddcd97acac124324f1c223f4173d
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-03-19 11:38:41 -0300

    Fix
    Some adjusts in  layout .

--- app/Http/Controllers/ProductImages/ProductImagesController.php
+++ app/Http/Controllers/ProductImages/ProductImagesController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers\ProductImages;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Product;
 use App\Models\ProductImages;
 use Exception;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Models\User;
 
@@ -15,23 +13,25 @@ class ProductImagesController extends Controller
 {
     //
     protected $user;
-    public function __construct(User $user){
+    public function __construct(User $user)
+    {
         $this->user = $user;
     }
-    public function index(){
+    public function index()
+    {
         $images = ProductImages::join('products', 'product_images.product_id', '=', 'products.id')
             ->select('product_images.*', 'products.name as product_name')
             ->get();
-          
+
         return response()->json($images);
     }
     public function store($upload_file, $product_id, $user_id)
     {
         try {
             foreach ($upload_file as $image) {
-               
+
                 $path_parts = pathinfo($image);
-                
+
                 $name_file = $path_parts['filename']; // Contém o nome do arquivo (sem a extensão)
                 $extension = $path_parts['extension']; // Contém a extensão do arquivo
 
@@ -42,67 +42,65 @@ class ProductImagesController extends Controller
                     'product_id' => $product_id,
                     'user_id' => $user_id,
                 ]);
-
             }
             return response()->json($image);
         } catch (Exception $e) {
             return response()->json($e);
         }
     }
-    public function update($upload_file, $id,Request $request){
-        
-        try{
+    public function update($upload_file, $id, Request $request)
+    {
+
+        try {
             $imagesProducts = ProductImages::where('product_id', $id)->get();
-                foreach ($upload_file as $image) {
-                    foreach($imagesProducts as $productImg){
-                        $path_parts = pathinfo($image);
-                    
-                        $name_file = $path_parts['filename']; // Contém o nome do arquivo (sem a extensão)
-                        $extension = $path_parts['extension']; // Contém a extensão do arquivo
-                        $updateProduct = ProductImages::where('id', $productImg['id'])->delete();
-                     }
-                    $product =  ProductImages::create([
-                        'name' => $name_file,
-                        'extension' => $extension,
-                        'size' => '2mb',
-                        'product_id' => $id,
-                        'user_id' => $request->user_id
-                    ]); 
+            foreach ($upload_file as $image) {
+                foreach ($imagesProducts as $productImg) {
+                    $path_parts = pathinfo($image);
+
+                    $name_file = $path_parts['filename']; // Contém o nome do arquivo (sem a extensão)
+                    $extension = $path_parts['extension']; // Contém a extensão do arquivo
+                    $updateProduct = ProductImages::where('id', $productImg['id'])->delete();
                 }
-             
-             return response()->json($product);
-        }
-        catch(Exception $e){
-            return response()->json($e);
-        }
-    }
-    public function destroy($id){
-        try{
-            $image = ProductImages::where('product_id')->get();
-            foreach($image as $img){
-                if($img){
-                    $delete_file = Storage::delete('./storage/products/'.$img->name);
-                    $delete_register = $image->delete();
-                 }
+                $product =  ProductImages::create([
+                    'name' => $name_file,
+                    'extension' => $extension,
+                    'size' => '2mb',
+                    'product_id' => $id,
+                    'user_id' => $request->user_id
+                ]);
             }
-            return response()->json($image); 
-        }
-        catch(Exception $e){
+
+            return response()->json($product);
+        } catch (Exception $e) {
             return response()->json($e);
         }
     }
-    public function destroyAll($id){
-        try{
+    public function destroy($id)
+    {
+        try {
+            $image = ProductImages::where('product_id')->get();
+            foreach ($image as $img) {
+                if ($img) {
+                    $delete_file = Storage::delete('./storage/products/' . $img->name);
+                    $delete_register = $image->delete();
+                }
+            }
+            return response()->json($image);
+        } catch (Exception $e) {
+            return response()->json($e);
+        }
+    }
+    public function destroyAll($id)
+    {
+        try {
             $image = ProductImages::where('product_id', $id)->get();
-           
-            foreach($image as $img){
+
+            foreach ($image as $img) {
                 $img->delete();
             }
             return response()->json($img);
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             return response()->json($e);
         }
-       
     }
 }
