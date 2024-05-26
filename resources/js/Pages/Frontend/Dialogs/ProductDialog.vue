1c339cf2134048b0bec9b2a0fd465eb8aeb4ffbf
rafaelPadua001 <145411939+rafaelPadua001@users.noreply.github.com>
2024-05-26 14:44:23 -0300

    FIx
    Colors and sizes

--- resources/js/Pages/Frontend/Dialogs/ProductDialog.vue
+++ resources/js/Pages/Frontend/Dialogs/ProductDialog.vue
@@ -33,8 +33,8 @@
                     <v-col cols="auto" md="4" sm="5">
                         <v-sheet class="ma-2 pa-2">
                             <v-row no-gutters>
-                                <v-col class="d-flex flex-column" v-for="(image, index) in JSON.parse(selectProduct.images)"
-                                    cols="2" md="3" sm="3">
+                                <v-col class="d-flex flex-column"
+                                    v-for="(image, index) in JSON.parse(selectProduct.images)" cols="2" md="3" sm="3">
                                     <v-hover v-slot="{ isHovering, props }" open-delay="500">
                                         <v-card width="50" class="mx-auto" v-bind="props"
                                             :color="isHovering ? 'cyan-darken-4' : undefined" elevation="0">
@@ -59,7 +59,8 @@
                                 </v-col>
 
                                 <v-col cols="12">
-                                    <v-card class="mx-auto" :min-width="150" :max-width="1500" :height="400" elevation="0">
+                                    <v-card class="mx-auto" :min-width="150" :max-width="1500" :height="400"
+                                        elevation="0">
                                         <div v-for="(image, index) in JSON.parse(selectProduct.images)" :key="index"
                                             class="image-container">
                                             <v-img v-if="index === selectImageIndex"
@@ -86,10 +87,10 @@
                     </v-col>
 
                     <v-col cols="auto" md="4" sm="6">
-                        <p justify="start" class="text-h5 bg-grey-lighten-4">
+                        <p justify="start" class="text-h5">
                             {{ selectProduct.name }}
                         </p>
-                        <v-divider> </v-divider>
+
                         <v-spacer></v-spacer>
                         <v-spacer></v-spacer>
                         <v-spacer></v-spacer>
@@ -112,6 +113,8 @@
                         <p color="red" v-else>
                             <strong> Fora de Estoque </strong>
                         </p>
+
+                        <v-divider></v-divider>
                         <div v-if="selectProduct.availability == 1" justify="start">
                             <v-responsive class="mx-auto">
                                 <v-rating v-model="rating" bg-color="orange-lighten-1" color="blue" size="x-small">
@@ -130,44 +133,80 @@
                             <p>Colors:</p>
                             <v-row no-gutters>
                                 <v-col class="d-flex flex-column" cols="auto" md="2" sm="3"
-                                    v-for="(color, index) in JSON.parse(selectProduct.colors)" :key="index">
+                                    v-for="(color, index) in parsedColors" :key="index">
                                     <v-hover>
                                         <template v-slot:default="{ isHovering, props }">
-                                            <v-card @click="getColors(color)" v-bind="props" :bg-color="color"
-                                                :color="isHovering ? undefined : color" :width="60">
-                                                <template v-slot:append>
+                                            <div v-if="parsedQuantityColors[index] >= 1">
+                                                <v-avatar @click="getColors(color)" v-bind="props" :bg-color="color"
+                                                    :color="isHovering ? undefined : color" :width="60" rounded="10">
+                                                    <template v-slot:append>
+                                                        <!-- {{ parsedQuantityColors[index] }} -->
+                                                        <!-- <span>Available</span> -->
+                                                    </template>
+                                                </v-avatar>
+                                            </div>
+                                            <div v-else>
+                                                <v-avatar @click="outOfStock()" v-bind="props" :bg-color="color"
+                                                    :color="isHovering ? undefined : color" :width="60" rounded="10">
+                                                    <span>
+                                                        <v-icon icon="fas fa-close" class="mr-1" size="sm"></v-icon>
+                                                    </span>
+                                                </v-avatar>
+                                            </div>
+                                        </template>
+                                    </v-hover>
+                                </v-col>
+                            </v-row>
+                        </div>
 
-                                                </template>
-                                            </v-card>
+                        <v-spacer></v-spacer>
+                        <v-spacer></v-spacer>
+                        <div v-if="selectProduct.size != 'null'">
+                            <p>Size:</p>
+                            <v-row fluid>
+                                <v-col cols="2" sm="2" md="2" v-for="(size, index) in parsedSizes" :key="index">
+                                    <v-hover>
+                                        <template v-slot:default="{ isHovering, props }">
+                                            <div v-if="parsedQuantitySize[index] >= 1">
+                                                <v-avatar v-bind="props" :width="40" color="grey">
+                                                        {{ size }}
+                                                </v-avatar>
+                                            </div>
+                                            <div v-else>
+                                                <v-row fluid>
+                                                    <v-col cols="2" sm="2" md="2">
+                                                        <v-avatar @click="outOfStock()" v-bind="props" :bg-color="color"
+                                                    :color="isHovering ? undefined : color" :width="60" rounded="10">
+                                                    <span>
+                                                        <v-icon icon="fas fa-close" class="mr-1" size="sm"></v-icon>
+                                                    </span>
+                                                </v-avatar>
+                                                    </v-col>
+                                                </v-row>
+                                            </div>
                                         </template>
                                     </v-hover>
 
                                 </v-col>
                             </v-row>
                         </div>
-
-                        <v-spacer></v-spacer>
-                        <v-spacer></v-spacer>
-
-                        <div v-if="selectProduct.size >= 1">
+                        <div v-else>
                             <p>Size:</p>
-                            <v-row>
-                                <v-col cols="2" sm="2" md="2" v-for="(size, index) in JSON.parse(selectProduct.size)"
-                                    :key="index">
-                                    <v-card :color="color" :width="40">
-                                        <template v-slot:append>
-                                            {{ size }}
-                                        </template>
+                            <v-row fluid>
+                                <v-col cols="2" sm="2" md="2">
+                                    <v-card @click="outOfStock()" :width="60">
+                                        Unique
                                     </v-card>
                                 </v-col>
                             </v-row>
                         </div>
 
+
                         <v-spacer></v-spacer>
                         <v-spacer></v-spacer>
 
                         <div>
-                            <v-row no-gutters>
+                            <v-row fluid>
                                 <v-col cols="auto" md="4" sm="6">
                                     <v-text-field v-model="quantity" label="Quantity" :placeholder="1">
                                         <template v-slot:append>
@@ -188,8 +227,8 @@
                         </div>
 
                         <div>
-                            <ZipCodeField :selectProduct="selectProduct" :quantity="this.quantity" :customer="this.customer"
-                                @updateShippment="updateShippment" />
+                            <ZipCodeField :selectProduct="selectProduct" :quantity="this.quantity"
+                                :customer="this.customer" @updateShippment="updateShippment" />
                         </div>
 
 
@@ -208,7 +247,8 @@
                                     @click="addItem(selectProduct)">
                                     <v-icon icon="fas fa-cart-plus" size="large"></v-icon>Carrinho
                                     <template v-slot:loader>
-                                        <v-progress-circular indeterminate text="teste"> Loading ...</v-progress-circular>
+                                        <v-progress-circular indeterminate text="teste"> Loading
+                                            ...</v-progress-circular>
 
                                     </template>
 
@@ -245,10 +285,8 @@
                                 <v-divider></v-divider>
                                 <v-card-text>
                                     <div>
-                                        <CommentsField 
-                                            :customer="this.customer"
-                                            :product="selectProduct"
-                                            @create-comment="newComment"/>
+                                        <CommentsField :customer="this.customer" :product="selectProduct"
+                                            @create-comment="newComment" />
                                     </div>
                                     <v-divider></v-divider>
                                     <div>
@@ -261,7 +299,8 @@
                                                             <v-toolbar-title>
                                                                 <v-avatar color="surface-variant" v-if="comment.name">
                                                                     <template v-slot="append">
-                                                                        <v-img :src="`./storage/avatars/${comment.name}`"
+                                                                        <v-img
+                                                                            :src="`./storage/avatars/${comment.name}`"
                                                                             :lazy-src="`./storage/avatars/${comment.name}`"
                                                                             cover>
 
@@ -271,21 +310,14 @@
                                                                 {{ comment.first_name }} {{ comment.last_name }}
 
                                                             </v-toolbar-title>
-                                                            <v-btn
-                                                                v-if="comment.user_id === customer.id"
-                                                                class="me-2"
-                                                                icon
-                                                                size="x-small"
-                                                                variant="plain"
-                                                                @click="remove(comment)"
-                                                            >
-                                                                    <v-icon
-                                                                        icon="fas fa-trash"
-                                                                    >
-                                                                    </v-icon>
-                                                                    
+                                                            <v-btn v-if="comment.user_id === customer.id" class="me-2"
+                                                                icon size="x-small" variant="plain"
+                                                                @click="remove(comment)">
+                                                                <v-icon icon="fas fa-trash">
+                                                                </v-icon>
+
                                                             </v-btn>
-                                                            
+
                                                         </v-toolbar>
 
                                                     </v-card-title>
@@ -297,9 +329,9 @@
                                                     <v-card-text align="right">
                                                         {{ comment.created_at.substr(0, 10) }}
                                                     </v-card-text>
-                                                    
+
                                                     <v-divider></v-divider>
-                                                    
+
                                                 </v-card>
                                             </v-list-item>
                                         </v-list>
@@ -320,7 +352,8 @@
                     </div>
                     <template v-slot:actions>
                         <v-btn-group>
-                            <v-btn size="small" variant="plain" color="white" @click="this.snackbar = false">Close</v-btn>
+                            <v-btn size="small" variant="plain" color="white"
+                                @click="this.snackbar = false">Close</v-btn>
                             <v-btn size="small" variant="plain" color="white" :to="`/login`">Login</v-btn>
                         </v-btn-group>
                     </template>
@@ -329,16 +362,11 @@
             </div>
         </v-card>
         <div>
-            <RemoveDialog 
-                v-model="removeDialog"
-                v-if="removeDialog"
-                :comment="removeComment"
-                @remove-comment="deleteComment"
-                @close-dialog="removeDialog = false"
-            />
+            <RemoveDialog v-model="removeDialog" v-if="removeDialog" :comment="removeComment"
+                @remove-comment="deleteComment" @close-dialog="removeDialog = false" />
         </div>
         <div>
-            <MenuBottomSheet v-model="bottomMenu" v-if="bottomMenu" :icons="this.social_icons"/>
+            <MenuBottomSheet v-model="bottomMenu" v-if="bottomMenu" :icons="this.social_icons" />
         </div>
 
     </v-dialog>
@@ -385,8 +413,22 @@ export default {
         delivery_name: false,
         removeDialog: false,
         removeComment: {},
-        
+
     }),
+    computed: {
+        parsedColors() {
+            return JSON.parse(this.selectProduct.colors);
+        },
+        parsedQuantityColors() {
+            return JSON.parse(this.selectProduct.color_quantity);
+        },
+        parsedSizes() {
+            return JSON.parse(this.selectProduct.size);
+        },
+        parsedQuantitySize() {
+            return JSON.parse(this.selectProduct.size_quantity);
+        }
+    },
     watch: {
         buyDialog(val) {
             val || this.closeBuy();
@@ -444,7 +486,7 @@ export default {
                 .then((response) => {
                     this.$emit('close-dialog');
                     return this.cart.push(response.data)
-                    
+
                 })
                 .catch((response) => {
                     this.snackbar = true;
@@ -459,6 +501,9 @@ export default {
         },
         getColors(color) {
             this.colors = color;
+        },
+        outOfStock() {
+            alert('this color is out of stock');
         },
         closeBuy() {
             this.$emit('update:buyDialog', false);
@@ -509,15 +554,15 @@ export default {
                     return alert('Error' + response);
                 });
         },
-        newComment(item){
+        newComment(item) {
             return this.comments.push(item);
         },
-        remove(item){
+        remove(item) {
             this.removeComment = Object.assign({}, item);
             this.removeDialog = true;
             console.log(this.removeComment);
         },
-        deleteComment(item){
+        deleteComment(item) {
             return this.comments.splice(item, 1);
         },
         openBottomMenu() {
